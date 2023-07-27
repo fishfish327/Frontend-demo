03c47831122abfb007b38f4877d5a3768b444e15
Ted Tong <tedtong@gmail.com>
2023-07-27 16:36:12 -0700

    Add basic EJS integration with stream

--- express-experiment/index.ts
+++ express-experiment/index.ts
@@ -1,12 +1,22 @@
 import express, { Express, Request, Response } from 'express';
+import stream from 'express-stream';
 
 
 const app: Express = express();
+app.set("view engine", "ejs");
+
 const port = 8080;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+
+stream.streamBefore('header');
+
+const homepageHandler = (req: Request, res: Response) => {
+  setTimeout(() => {
+    res.render("pages/index");
+  }, 5000);
+}
+
+app.get('/', stream.stream(), homepageHandler);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
