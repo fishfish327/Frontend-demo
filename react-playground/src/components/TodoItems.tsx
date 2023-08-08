0924261d765a63bc45e87c3464a8c264201b4c99
Ted Tong <tedtong@gmail.com>
2023-08-07 17:50:38 -0700

    Make changes on delete and status update

--- react-playground/src/components/TodoItems.tsx
+++ react-playground/src/components/TodoItems.tsx
@@ -1,12 +1,30 @@
 import "./TodoList.css";
 
-const TodoItem = (item: any, removalHandler: any) => {
-    return <li key={item.key} onClick={() => removalHandler(item.key)}>{item.text}</li>;
+export interface Item {
+    key: number;
+    text: string;
+    isCompleted: boolean;
+}
+
+const TodoItem = (item: Item, removalHandler: any, statusChangeHandler: any) => {
+    const onCheckBox = (e: any, statusChangeHandler: any, item: Item) => {
+        e.stopPropagation();
+        statusChangeHandler(item);
+    }
+    return (
+            <li key={item.key} className={item.isCompleted ? "completed" : ""}>
+                <input type="checkbox" checked={item.isCompleted} 
+                    onChange={(e) => onCheckBox(e, statusChangeHandler, item)}>
+                </input>
+                {item.text}
+                <button onClick={() => removalHandler(item)}>Delete</button>
+            </li>
+        );
 }
 
 const TodoItems = (props: any) => {
     const todoEntries = props.todoEntries;
-    const todoItems = todoEntries.map((item: any) => TodoItem(item, props.removalHandler));
+    const todoItems = todoEntries.map((item: any) => TodoItem(item, props.removalHandler, props.statusChangeHandler));
     return (
         <ul className="theList">
             {todoItems}
--- react-playground/src/components/TodoList.tsx
+++ react-playground/src/components/TodoList.tsx
@@ -1,21 +1,35 @@
 import { useState } from 'react';
 import TodoItems from './TodoItems';
+import { Item } from './TodoItems';
 import "./TodoList.css";
 
 const TodoList = () => {
-    const [todoItems, setTodoItems] = useState<any[]>([]);
+    const [todoItems, setTodoItems] = useState<Item[]>([]);
     const onSubmit = (event: any) => {
         event.preventDefault();
         const item = {
             key: Date.now(),
-            text: event.target[0].value
+            text: event.target[0].value, 
+            isCompleted: false
         }
-        setTodoItems(todoItems.concat(item));
+        setTodoItems(todoItems.concat(item as Item));
     };
 
-    const removeItem = (key: any) => {
-        setTodoItems(todoItems.filter(item => item.key !== key));
+    const removeItem = (itemToDelete: Item) => {
+        setTodoItems(todoItems.filter(item => item.key !== itemToDelete.key));
     };
+
+    const onStatusChange = (itemToUpdate: Item) => {
+        const newItem = {
+            key: Date.now(),
+            text: itemToUpdate.text,
+            isCompleted: !itemToUpdate.isCompleted
+        };
+        const newItemList = todoItems.map(item => {
+            return item.key === itemToUpdate.key ? newItem : itemToUpdate;
+        });
+        setTodoItems(newItemList);
+    }
 
     return (
         <div className='todoListMain'>
@@ -25,7 +39,7 @@ const TodoList = () => {
                     <button type="submit">Add</button>
                </form>
             </div>
-            <TodoItems todoEntries={todoItems} removalHandler={removeItem}/>
+            <TodoItems todoEntries={todoItems} removalHandler={removeItem} statusChangeHandler={onStatusChange}/>
         </div>
     );
 }
